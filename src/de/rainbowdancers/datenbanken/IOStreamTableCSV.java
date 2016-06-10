7de8a803a23cf7df23b203f4b1e91f2fc85a62b0
Tobi Wan <Tobi.Klatt@web.de>
2016-06-10 11:25:57 +0200

    Problemen behoben. Code ist lauffähig

---
+++ src/de/rainbowdancers/datenbanken/IOStreamTableCSV.java
@@ -0,0 +1,30 @@
+package de.rainbowdancers.datenbanken;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import de.tobi_wan.support.IOStreamCSV;
+import de.tobi_wan.support.Table;
+
+
+
+public class IOStreamTableCSV extends IOStreamCSV {
+   // Attribute
+
+   // Konstruktor
+   public IOStreamTableCSV(String separator) {
+      super(separator);
+   }
+
+   // Getter & Setter
+
+   // Methoden
+   public Table readCSVIntoTable(String path, Table table) throws IOException {
+      // Sicherheitsabfrage
+      List<String> lines = Files.readAllLines(Paths.get(path));
+      for (String line : lines)
+         table.addRow(line.split(getSeparator()));
+      return table;
+   }
+}
---
+++ src/de/rainbowdancers/datenbanken/Test.java
@@ -0,0 +1,42 @@
+package de.rainbowdancers.datenbanken;
+
+import java.io.IOException;
+import de.tobi_wan.support.StandardOutput;
+import de.tobi_wan.support.Table;
+
+public class Test {
+
+   public static void main(String [] args) {
+      // Attribute
+      StandardOutput s = StandardOutput.defaultSupportMethods();
+      String connectionString = "jdbc:oracle:thin:@dbl43.beuth-hochschule.de:1521:oracle";
+      String user = "KLATT";
+      String password = "Student";
+      DatabaseOperationsOracle dbo = new DatabaseOperationsOracle(connectionString, user, password);
+      IOStreamTableCSV io = new IOStreamTableCSV(";");
+      TranslateTableToSQL CSVToSQL = new TranslateTableToSQL();
+      Table brands = new Table("Brands", 2);
+      Table clothing = new Table("Clothing", 2);
+      Table colors = new Table("Colors", 2);
+      Table onlineShops = new Table("OnlineShops", 2);
+      Table outfits = new Table("Outfits", 6);
+
+      dbo.printFields();
+      // dbo.connect();
+      try {
+         io.readCSVIntoTable("data/Brands.csv", brands);
+         io.readCSVIntoTable("data/Clothing.csv", clothing);
+         io.readCSVIntoTable("data/Colors.csv", colors);
+         io.readCSVIntoTable("data/OnlineShops.csv", onlineShops);
+         io.readCSVIntoTable("data/Outfit.csv", outfits);
+         s.println("Tabellen eingelesen");
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      // Tabellen in SQL uebersetzen
+      // Ggf. Tabelle erzeugen
+      // SQL Transaktionen anweisen
+      // dbo.disconnect();
+   }
+
+}
